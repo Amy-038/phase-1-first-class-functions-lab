f5c0db58600bdbd5facf62a9d313850b33d9dd1f
Amy-038 <amy.sirma@student.moringaschool.com>
2023-12-13 14:50:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 
-function returnFirstTwoDrivers() {
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0,2)
+}
+/*const returnFirstTwoDrivers = function(array) {
+    return array.slice(0,2)
+}
+returnFirstTwoDrivers(drivers);*/
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return function(fare) {
+        return fare * int;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+console.log(fareDoubler(5));
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, anyfn) {
+    return anyfn();
+}
+selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+
+
+/*function returnFirstTwoDrivers() {
     return drivers.slice(0,2);
 }
 
@@ -24,4 +56,4 @@ const fareTripler = createFareMultiplier(3);
 
 function selectDifferentDrivers(selectingDrivers, driver) {
     return driver();
-}
+}*/
